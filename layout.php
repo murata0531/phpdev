e4d386afa87ac5a20c2b54ea899efb0205a0c969
murata0531 <coless115@gmail.com>
2021-02-08 02:15:27 +0900

    a

--- layout.php
+++ layout.php
@@ -37,6 +37,11 @@
                     color:black;
                 }
 
+                .con5 {
+                    background:gray;
+                    color:black;
+                }
+
             </style>
 
         </head>
@@ -133,5 +138,16 @@
                 </div>
             </div>
     
+            <div class="container px-4 con5">
+                <div class="row gx-5">
+                    <div class="col">
+                        <div class="p-3 border bg-light">Custom column padding</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3 border bg-light">Custom column padding</div>
+                    </div>
+                </div>
+            </div>
+
         </body>
     </html>
