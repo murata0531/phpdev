aae1425b04accbd1c2b5e2445a3e1643498fb33c
murata0531 <coless115@gmail.com>
2021-02-28 15:15:43 +0900

    b

---
+++ databasetop.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>掲示板</title>
+</head>
+<body>
+    <h1>テニスサークル交流サイト</h1>
+    <p><a href="databasealubum.php"></a></p>
+    <p><a href="database.php"></a></p>
+    <h2>お知らせ</h2>
+</body>
+</html>
